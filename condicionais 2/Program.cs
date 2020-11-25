90d0454802c68c28d041d36788bb23fad5bac3f4
Ester <ester.albuquerque500@gmail.com>
2020-11-25 11:34:36 -0300

    Exercicio 2

---
+++ condicionais 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace condicionais_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Calculadora");
+           Console.WriteLine("----------------");
+
+        //ENTRADA
+           Console.WriteLine("Digite o ano que você nasceu: ");
+           int anonascimento = int.Parse(Console.ReadLine()); 
+
+           Console.WriteLine("Digite o ano atual: ");
+           int anoatual = int.Parse(Console.ReadLine());
+
+        //PROCESSAMENTO
+            int idade = anoatual- anonascimento;
+            int semanas = idade * 52;
+        
+        //EXIBIR
+
+         System.Console.WriteLine("A sua idade é: "+idade);
+          System.Console.WriteLine("Semanas vividas é: "+semanas);
+
+
+
+
+
+
+        }
+    }
+}
